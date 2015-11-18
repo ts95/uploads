3ae56b3da0bcb669bf0feadfd5767e2638c1846a
Toni S <toni.sucic@icloud.com>
2015-11-18 19:22:07 +0100

    Refactor the whole project
    Use the file-type module to figure out the file type as it’s more
    reliable than multer.
    
    Support mp4 files.
    
    Add delete route.

--- index.js
+++ index.js
@@ -1,64 +1,121 @@
-var Promise 	= require('bluebird');
-var express 	= require('express');
-var multer 		= require('multer')
-var fs 			= Promise.promisifyAll(require('fs'));
-var fileHash 	= require('./lib/file-hash');
+var Promise 		= require('bluebird');
+var express 		= require('express');
+var multer 			= require('multer');
+var bodyParser 		= require('body-parser');
+var fileType 		= require('file-type');
+var readChunkAsync 	= Promise.promisify(require('read-chunk'));
+var fs 				= Promise.promisifyAll(require('fs'));
+var fileHash 		= require('./lib/file-hash');
 
 var imageDir = __dirname + '/images';
-var passcode = '[some unique code]';
+var passcode = process.env.IMG_SERVICE_PASSCODE || 'ayy lmao';
+var port = process.env.IMG_SERVICE_PORT || 8000;
+var forceHTTPS = true;
+
+function fail(res, err) {
+	console.error(err);
+	console.trace();
+
+	res.writeHead(500);
+	res.end("Error: " + err.message);
+}
 
 var app = express();
 
 app.use('/i', express.static(imageDir));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 var upload = multer({ dest: imageDir });
 
 app.get('/', function(req, res) {
-	res.send('img-service is running. More info at https://github.com/ts95/img-service');
+	res.send('img-service is running');
 });
 
 app.post('/upload', upload.single('image'), function(req, res) {
-	if (req.body.passcode !== passcode) {
-		res.writeHead(500);
-		res.end("Error: Invalid passcode");
-		return;	
-	}
-
-	var validTypes = ['image/gif', 'image/jpeg', 'image/png', 'video/webm'];
-
-	if (!~validTypes.indexOf(req.file.mimetype)) {
-		res.writeHead(500);
-		res.end("Error: Invalid MIME-type");
-		return;
-	}
-
-	var ext = '.' + req.file.mimetype.split('/')[1];
-
-	var filename = imageDir + '/' + req.file.filename;
-
-	fileHash(fs.createReadStream(filename))
-		.then(function(hash) {
-			var uploadedFilename = hash + ext;
-			var destFilename = imageDir + '/' + uploadedFilename;
-			if (!fs.existsSync(destFilename)) {
-				fs.createReadStream(filename).pipe(fs.createWriteStream(destFilename));
+	Promise.resolve()
+		.then(function() {
+			if (req.body.passcode !== passcode) {
+				return Promise.reject(new Error("Invalid passcode"));
 			}
-			return fs.unlinkAsync(filename)
-				.then(function() {
+
+			if (!req.file) {
+				return Promise.reject(new Error("No file was uploaded"));
+			}
+
+			if (req.file.size < 262) {
+				return Promise.reject(new Error("File too small"));
+			}
+
+			return readChunkAsync(req.file.path, 0, 262);
+		})
+		.then(function(buffer) {
+			return Promise.resolve(fileType(buffer));
+		})
+		.then(function(type) {
+			var validExts = ['gif', 'jpg', 'png', 'webm', 'mp4'];
+
+			if (!~validExts.indexOf(type.ext)) {
+				return Promise.reject(
+					new Error("Invalid file type: " + type.mime + " (" + type.ext + ")")
+				);
+			}
+
+			return fileHash(fs.createReadStream(req.file.path))
+				.then(function(hash) {
+					var uploadedFilename = hash + '.' + type.ext;
+					var destFilename = imageDir + '/' + uploadedFilename;
+					if (!fs.existsSync(destFilename)) {
+						fs.createReadStream(req.file.path).pipe(fs.createWriteStream(destFilename));
+					}
 					return Promise.resolve(uploadedFilename);
 				});
 		})
 		.then(function(uploadedFilename) {
-			res.send(req.protocol + '://' + req.get('host') + '/i/' + uploadedFilename);
+			console.log(uploadedFilename + " successfully uploaded");
+			res.send(
+				(forceHTTPS ? 'https' : req.protocol) + '://' + req.get('host') + '/i/' + uploadedFilename
+			);
 		})
 		.catch(function(err) {
-			console.error(err);
-			res.writeHead(500);
-			res.end('Error: ' + err.message);
+			fail(res, err);
+		})
+		.finally(function() {
+			fs.unlinkAsync(req.file.path)
+				.catch(function() {
+					console.error("Failed to unlink file: " + req.file.path);
+				});
 		});
 });
 
-var server = app.listen(8000, function () {
+app.post('/delete', function(req, res) {
+	var filename = imageDir + '/' + req.body.filename;
+
+	Promise.resolve()
+		.then(function() {
+			if (req.body.passcode !== passcode) {
+				return Promise.reject(new Error("Invalid passcode"));
+			}
+
+			if (!fs.existsSync(filename)) {
+				return Promise.reject(new Error("This file doesn't exist"));
+			}
+
+			return Promise.resolve();
+		})
+		.then(function() {
+			return fs.unlinkAsync(filename);
+		})
+		.then(function() {
+			var msg = filename + " successfully deleted";
+			console.log(msg);
+			res.send(msg);
+		})
+		.catch(function(err) {
+			fail(res, err);
+		});
+});
+
+var server = app.listen(port, function () {
 	var host = server.address().address;
 	var port = server.address().port;
 
--- readme.md
+++ readme.md
@@ -1,3 +1,17 @@
 img-service
 ========
 A simple image hosting service.
+
+### img-service supports the following file types
+* png
+* jpg
+* gif
+* webm
+
+### Automatic conversion
+If you upload a mov file it will be converted to a webm file.
+
+# Routes
+* GET   /
+* POST  /upload
+* POST  /delete
